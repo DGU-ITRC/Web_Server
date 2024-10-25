be7c7dd6bab27270e5c44404e08e5b4fb4fd8f2c
sucystem <sucystem@outlook.com>
2024-10-25 14:56:25 +0900

    comment cleanup

--- modules/crawl.js
+++ modules/crawl.js
@@ -7,6 +7,10 @@ async function crawlNews() {
         args: ['--no-sandbox', '--disable-setuid-sandbox']
     });
     const page = await browser.newPage();
+
+    //로그 찍을때
+    //page.on('console', (msg) => console.log(msg.text()));
+
     const query = '노인 복지';
     let articles = [];
 
@@ -14,7 +18,8 @@ async function crawlNews() {
         const url = `https://search.naver.com/search.naver?where=news&sm=tab_jum&query=${encodeURIComponent(query)}&start=${pageIndex * 10 - 9}`;
 
         await page.goto(url, { waitUntil: 'networkidle2' });
-        await page.waitForSelector('.dsc_thumb img', { visible: true });
+        await page.waitForSelector('.dsc_thumb img');
+        await new Promise((page) => setTimeout(page, 30000));
 
         const newArticles = await page.evaluate(() => {
             const results = [];
@@ -22,23 +27,11 @@ async function crawlNews() {
 
             items.forEach(item => {
                 const title = item.querySelector('.news_tit')?.textContent;
-                let thumb = null;
 
-                /*
-                // 두 번째 img 태그에서 썸네일을 가져옴
-                const imgs = item.querySelectorAll('img');
-                if (imgs.length > 1) {
-                    let thumbUrl = imgs[1].src;
-                    if (thumbUrl) {
-                        // 확장자까지 잘라내기 (type= 부분 제거)
-                        const match = thumbUrl.match(/(.*\.(jpg|jpeg|png|gif))/i);
-                        if (match) {
-                            thumb = match[1];
-                        }
-                    }
-                }
-                    */
+                //썸네일
+                let thumb = null;
                 const thumbElement = item.querySelector('.dsc_thumb img');
+                
                 if (thumbElement) {
                     thumb = thumbElement.src;
 
@@ -81,28 +74,6 @@ async function crawlNews() {
                         created = Date.now() - secondsAgo * 1000;
                     }
                 }
-
-                /*
-                // "몇 시간 전"을 계산해서 created 값을 설정
-                let relativeTime = item.querySelector('.info')?.innerText;
-                let created = Date.now();
-
-                if (relativeTime) {
-                    if (relativeTime.includes('일 전')) {
-                        const daysAgo = parseInt(relativeTime.match(/\d+/)?.[0], 10);
-                        created = Date.now() - daysAgo * 24 * 60 * 60 * 1000;
-                    } else if (relativeTime.includes('시간 전')) {
-                        const hoursAgo = parseInt(relativeTime.match(/\d+/)?.[0], 10);
-                        created = Date.now() - hoursAgo * 60 * 60 * 1000;
-                    } else if (relativeTime.includes('분 전')) {
-                        const minutesAgo = parseInt(relativeTime.match(/\d+/)?.[0], 10);
-                        created = Date.now() - minutesAgo * 60 * 1000;
-                    } else if (relativeTime.includes('초 전')) {
-                        const secondsAgo = parseInt(relativeTime.match(/\d+/)?.[0], 10);
-                        created = Date.now() - secondsAgo * 1000;
-                    }
-                }
-                    */
 
                 // 언론사 이름 가져오기
                 const media = item.querySelector('.info_group > a')?.textContent?.trim();
